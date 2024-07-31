f02f5f45a3c922b328cdd0ac6d2ede74130923ee
Chirana <engsw.thiago@gmail.com>
2024-07-30 21:06:46 -0300

    feat | add frontend

---
+++ app/frontend/src/components/auth/AuthLogin.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>OI CHEGAY TESTE DE VIEW</h1>
+</template>
+
+<script>
+console.log('HELLO WORLD')
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 800;
+  font-size: 2.6rem;
+  top: -10px;
+}
+</style>
---
+++ app/frontend/src/views/AuthView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AuthLogin from '../components/auth/AuthLogin.vue'
+</script>
+
+<template>
+  <main>
+    <AuthLogin />
+  </main>
+</template>
